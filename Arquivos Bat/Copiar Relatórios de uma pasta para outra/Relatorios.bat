b1209075cb44fa5b4ab0e3a142177a99f60bdb43
Beatriz <147112758+beatriznarf@users.noreply.github.com>
2025-10-07 20:47:08 -0300

    Update Relatorios.bat

--- Arquivos Bat/Copiar Relatórios de uma pasta para outra/Relatorios.bat
+++ Arquivos Bat/Copiar Relatórios de uma pasta para outra/Relatorios.bat
@@ -7,16 +7,11 @@ color 0B
 	
 		set "origemRelatorio=C:\Windows\Caminho\diretorio\Sistema\Relatorio\"
 
-
-
 		:: Aqui iremos definir o nome de outra variável e o diretório onde queremos que os arquivos copiados sejam colados
 			set "destino1=C:\Windows\Diretorio\Sistema2\Relatorio\"
 
-
-
 	:: Como desejo apenas copiar os relatórios (que possuem a extensão como ".fr3"), irem informar da seguinte forma a minha copia 
 			xcopy "%origemRelatorio%*.fr3" "%destino1%" /E /Y /C /H /D
-
 
 		:: xcopy: 
 			:: /E  - Copia diretórios e subdiretórios
@@ -25,17 +20,9 @@ color 0B
 			:: /H  - Copia arquivos ocultos e do sistema
 			:: /D  - Copia arquivos somente se a origem for mais recente que o destino
 
-
-
-
-			
-
 	:: Além disso, podemos fazer para vários caminho, veja um exemplo: 
 
 		set "origemRelatorio=C:\Windows\Caminho\diretorio\Sistema\Relatorio\"
-
-
-
 
 			set "destino1=C:\Windows\Diretorio1\Sistema\Relatorio\"
 			set "destino2=C:\Windows\Diretorio2\Sistema\Relatorio\"
@@ -45,7 +32,6 @@ color 0B
 			set "destino6=C:\Windows\Diretorio6\Sistema\Relatorio\"
 	
 						
-
 		:: Copia todos os arquivos .fr3 da pasta %origemRelatorio% para as pastas...
 
 			xcopy "%origemRelatorio%*.fr3" "%destino1%" /E /Y /C /H /D
